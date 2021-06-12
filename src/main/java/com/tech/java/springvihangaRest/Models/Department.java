19321495c327be927205764c698b95cfd94e58f3
VihangaT <liyanage.vihanga99@gmail.com>
2021-06-12 11:18:56 +0530

    api-02

--- src/main/java/com/tech/java/springvihangaRest/Models/Department.java
+++ src/main/java/com/tech/java/springvihangaRest/Models/Department.java
@@ -6,13 +6,16 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+
 @Entity
 public class Department {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long departmentID;
-    private  String departmentName;
+
+//    @NotBlank(message = "Please add Department Name")
+    private String departmentName;
     private String departmentAddress;
     private String departmentCode;
 
