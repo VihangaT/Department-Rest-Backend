1e90b88207728dad40a2afb9802a89736c00c60c
VihangaT <liyanage.vihanga99@gmail.com>
2021-06-13 19:35:59 +0530

    Start scripts added

---
+++ src/main/resources/start.bat
@@ -0,0 +1,4 @@
+@echo off
+title Department-Rest-Backend (Started at %Time% %Date% )
+
+java -jar "libs/spring-vihanga-Rest-1.0.0-Department-Rest.jar"
---
+++ src/main/resources/start.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+java -jar "libs/spring-vihanga-Rest-1.0.0-Department-Rest.jar"
